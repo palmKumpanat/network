d650e92487ef01b4722f3c2eb359ab38c36b0d75
palmKumpanat <kumpanat.joompabuth@gmail.com>
2022-05-27 12:58:19 +0700

    first commit

---
+++ client.java
@@ -0,0 +1,34 @@
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class client {
+    public static void main(String[] args) throws IOException{
+        Socket s = new Socket("172.17.0.1", 4999);
+        Scanner in = new Scanner(System.in);
+        System.out.println("------------ Kangaroo Words ------------");
+        System.out.print("==================================");
+        System.out.print("Enter Word : ");
+        String word1 = in.nextLine();
+        System.out.print("Enter word do you want to find : ");
+        String fWord = in.nextLine();
+        System.out.print("==================================");
+        PrintWriter p = new PrintWriter(s.getOutputStream());
+        p.println(word1);
+        p.flush();
+        p.println(fWord);
+        p.flush();
+        InputStreamReader i = new InputStreamReader(s.getInputStream());
+        BufferedReader Bf = new BufferedReader(i);
+        String a = Bf.readLine();
+        System.out.println("Server : "+ a);
+        s.close();
+        in.close();
+        p.close();
+        i.close();
+        Bf.close();
+    }
+}
---
+++ server.java
@@ -0,0 +1,52 @@
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+public class server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serv = new ServerSocket(4999);
+        Socket s = serv.accept();
+        System.out.println("client Connected");
+        InputStreamReader input = new InputStreamReader(s.getInputStream());
+        BufferedReader Bf = new BufferedReader((input));
+        String w1 = Bf.readLine();
+        String w2 = Bf.readLine();
+        System.out.print("==================================");
+        System.out.println("Entered Word From Client : " + w1);
+        System.out.println("Entered Word that Client want to find : " + w2);
+        System.out.print("==================================");
+        PrintWriter p = new PrintWriter(s.getOutputStream());
+        int i, j=0, k=0;
+        int c1 = w1.length();
+        int c2 = w2.length();
+        char[] z1 = w1.toCharArray();
+        char[] z2 = w2.toCharArray();
+
+        for(i=0;i<c1;i++){
+            if(z1[i] == z2[j]){
+                j++;
+                if(j == c2){
+                    k = 1;
+                }
+            }
+        }
+
+        if(k == 1){
+            p.println("There is a word \" " + w2 +  "\" hidden in the word \"" + w1 + "\".");
+        }
+        else{
+            p.println("The word \" " + w2 +  "\" is not hidden in the word \"" + w1 + "\".");
+        }
+    
+        p.flush();
+        serv.close();
+        s.close();
+        input.close();
+        Bf.close();
+        p.close();
+    }
+    
+}
